7e316a7380fd55b4af0d976a920835f22e9deddf
mahabub <mahabubalom72091@gmail.com>
2024-04-16 13:22:31 +0300

    adding

--- src/Components/Pages/Shrared/LeftSideNav/LeftSideNav.jsx
+++ src/Components/Pages/Shrared/LeftSideNav/LeftSideNav.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 
 const LeftSideNav = () => {
+        const [ categories, setCategories] = useState([])
+
+        useEffect (() => {
+                fetch('categories.json')
+                .then(res => res.json())
+                .then(data => setCategories(data))
+        } ,[])
+
+        
+
+        
         return (
-                <div>
-                      <h2 className="text-2xl">This Nav LeftSide</h2>  
+                <div className="space-y-6">
+                     <h2 className="text-2xl">All Categoreis</h2>
+                     {
+                        categories.map (category => <Link className="block ml-4 text-xl font-semibold "
+                         key={category.id} to={`/catrgory/${category.id}`}>{category.name}</Link>)
+                     }
+                     
                 </div>
         );
 };
