7339918c83afef1804b9e571c362f7b1ad0f2957
mahabub <mahabubalom72091@gmail.com>
2024-04-16 13:53:45 +0300

    adding

--- src/Components/Pages/Shrared/LeftSideNav/LeftSideNav.jsx
+++ src/Components/Pages/Shrared/LeftSideNav/LeftSideNav.jsx
@@ -2,25 +2,37 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 
-const LeftSideNav = () => {
-        const [ categories, setCategories] = useState([])
 
-        useEffect (() => {
-                fetch('categories.json')
-                .then(res => res.json())
-                .then(data => setCategories(data))
+const LeftSideNav = () => {
+
+        const [categories, setCategories] = useState ([])
+
+        useEffect(() =>{
+                fetch ('categories.json')
+                .then(res =>res.json())
+                .then(data =>setCategories(data))
         } ,[])
+
+        // const [ categories, setCategories] = useState([])
+
+        // useEffect (() => {
+        //         fetch('categories.json')
+        //         .then(res => res.json())
+        //         .then(data => setCategories(data))
+        // } ,[])
 
         
 
         
         return (
                 <div className="space-y-6">
-                     <h2 className="text-2xl">All Categoreis</h2>
-                     {
-                        categories.map (category => <Link className="block ml-4 text-xl font-semibold "
-                         key={category.id} to={`/catrgory/${category.id}`}>{category.name}</Link>)
-                     }
+                     <h2 className="text-2xl font-poppins font-bold text-center">All Categoreis</h2>
+                    
+                     
+                      {
+                        categories.map (category => <Link className="block ml-4 text-xl "
+                                 key={category.id} to= {`/catrgory/${category.id}`}>{category.name}</Link>)
+                      }
                      
                 </div>
         );
